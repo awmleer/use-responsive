53269f7a634af6de6799b7cd58c4ce7082ce363a
awmleer <awmleer@sparker.xyz>
2019-08-17 10:39:48 +0800

    init project

---
+++ src/__tests__/main.test.tsx
@@ -0,0 +1,7 @@
+import * as testing from '@testing-library/react'
+import {FC, useState} from 'react'
+import * as React from 'react'
+
+test('get async data', function () {
+  expect(1).toBe(1)
+})
